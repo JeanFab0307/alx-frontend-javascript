7610f5d5fb45849c571e1d9360070524f9abfd98
JeanFab0307 <jfrandriatsimivony@gamil.com>
2024-01-18 16:48:49 +0300

    Add other tasks

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
   for (const value of array) {
-    value = appendString + value;
+    const idx = array.indexOf(value);
+    arr[idx] = appendString + value;
   }
 
-  return array;
+  return arr;
 }
