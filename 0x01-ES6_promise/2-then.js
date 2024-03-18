0f2e85017a776163db0df228b06caab7c96d89ed
JeanFab0307 <jfrandriatsimivony@gamil.com>
2024-03-18 10:29:42 +0300

    task2 Catch me if you can

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  const resp = {
+    status: 200,
+    body: 'Success'
+  };
+  return promise
+    .then(() => resp)
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+
+}
