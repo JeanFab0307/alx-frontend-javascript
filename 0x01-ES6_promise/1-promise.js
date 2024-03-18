52552c293e7e1e55b69caca82fb3436d136e82d6
JeanFab0307 <jfrandriatsimivony@gamil.com>
2024-03-18 10:06:57 +0300

    Remove useless comments from file

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,17 +4,7 @@ export default function getFullResponseFromAPI(success) {
     body: 'Success',
   };
   const errMessage = new Error('The fake API is not working currently');
-  // const promise = new Promise((resolve, reject) => {
-  //   if (success) {
-  //     resolve(resp);
-  //   } else {
-  //     reject(Error(errMessage));
-  //   }
-  // });
-  // promise
-  //   .then((resp) => resp)
-  //   .catch((fail) => fail);
-
+  
   if (success)
     return Promise.resolve(resp);
   return Promise.reject(errMessage);
