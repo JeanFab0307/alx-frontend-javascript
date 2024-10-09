344c63d5db46a273e9df17020d8f2ec60348b409
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-10-09 14:22:17 +0300

    changed return value

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,15 @@ import uploadPhoto from './5-photo-reject'
 export default function handleProfileSignup(firstName, lastName, filename) {
   const user = signUpUser(firstName, lastName)
   const photo = uploadPhoto(filename)
+  const res = []
   
-  return Promise.allSettled([user, photo]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })))
+  return Promise.allSettled([user, photo]).then((results) => {
+    results.map((result) => (
+      res.push({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+      })
+    ));
+    return res;
+  });
 }
