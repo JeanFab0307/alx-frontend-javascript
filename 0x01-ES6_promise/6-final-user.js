e1252679883a055f950c907c085b2faced27b021
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-10-09 14:24:19 +0300

    correct lint error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const user = signUpUser(firstName, lastName)
-  const photo = uploadPhoto(filename)
-  const res = []
-  
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(filename);
+  const res = [];
+
   return Promise.allSettled([user, photo]).then((results) => {
     results.map((result) => (
       res.push({
