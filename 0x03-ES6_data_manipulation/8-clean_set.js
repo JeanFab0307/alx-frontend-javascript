a9627adb50d47e4958c87b4e1ac0d7dc0ef58640
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-10-22 13:16:18 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  if (!startString || !startString.trim()) {
+    return '';
+  }
+  // check if set is an instance of Set
+  if (!(set instanceof Set)) {
+    throw new Error('The "set" argumenent must be a Set');
+  }
+  let result = '';
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, -1);
+}
